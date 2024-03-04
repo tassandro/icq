3717b20c91745b11e7d1cbf7e06a25967a2040c9
tassandro <124645934+tassandro@users.noreply.github.com>
2024-03-04 12:12:13 -0300

    Update quest1.py

--- quest1.py
+++ quest1.py
@@ -1,6 +1,5 @@
-
 import math
-
+#a equação dada pode ser reduzida a uma equação do segundo grau usando a substituição x**2=y
 def solve_quadratic(a, b, c):
 
   discriminant = b**2 - 4*a*c
@@ -11,10 +10,17 @@ def solve_quadratic(a, b, c):
     root2 = (-b - math.sqrt(discriminant)) / (2*a)
     return root1, root2
 
-roots = solve_quadratic(1, 0, 1)
+roots = solve_quadratic(1, 2, 1)
 
 # Print the result
 if roots is None:
+  print("A equação y^2 + 2y + 1 = 0 não tem solução real.")
+else:
+  print("A equação y^2 + 2y + 1 = 0 tem solução real:, logo, x^2 - (",roots[0],")=0 e x^2 - (",roots[1],")=0.")
+
+roots1=solve_quadratic(1,0,1)
+if roots1 is None:
   print("A equação x^2 + 1 = 0 não tem solução real.")
 else:
-  print("A equação x^2 + 1 = 0 tem pelo menos uma solução real.", roots)
+  print("A equação x^2 + 1 = 0 tem pelo menos uma solução real:", roots1)
+
